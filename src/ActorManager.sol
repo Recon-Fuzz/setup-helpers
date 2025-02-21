06f8d463b006595109b504e3b0fd25c3c1a49c56
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-02-21 15:40:43 -0300

    refactor: change implementations to latest from create-chimera-app 2

--- src/ActorManager.sol
+++ src/ActorManager.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: GPL-2.0
 pragma solidity ^0.8.0;
 
-import {BaseSetup} from "chimera/BaseSetup.sol";
-import {vm} from "chimera/Hevm.sol";
-import {EnumerableSet} from "openzeppelin/utils/structs/EnumerableSet.sol";
-import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {BaseSetup} from "@chimera/BaseSetup.sol";
+import {vm} from "@chimera/Hevm.sol";
+import {EnumerableSet} from "./utils/EnumerableSet.sol";
 
+/// @dev This is the source of truth for the actors being used in the test
+/// @notice No actors should be used in the suite without being added here first
 abstract contract ActorManager {
     using EnumerableSet for EnumerableSet.AddressSet;
 
+    ///@notice The current actor being used
     address private _actor;
 
+    ///@notice The list of all actors being used
     EnumerableSet.AddressSet private _actors;
 
     // If the current target is address(0) then it has not been setup yet and should revert
@@ -22,55 +25,23 @@ abstract contract ActorManager {
     // Do not allow the default actor
     error DefaultActor();
 
-    // TODO: We have defined the library
-    // But we need to make this more explicit
-    // So it's very clean in the story what's going on
-
+    /// @notice address(this) is the default actor
     constructor() {
-        // address(this) is the default actor
         _actors.add(address(this));
         _actor = address(this);
     }
-    
-    modifier useActor() {
-        vm.prank(_getActor());
-        _;
-    }
 
-    // use this function to get the current active actor
+    /// @notice Returns the current active actor
     function _getActor() internal view returns (address) {
        return _actor;
     }
 
-    // returns an actor different from the currently set one 
-    function _getDifferentActor() internal view returns (address differentActor) {
-        address[] memory actors_ = _getActors();
-        for(uint256 i; i < actors_.length; i++) {
-            if(actors_[i] != _actor) {
-                differentActor = actors_[i];
-            }
-        }
-    }
-
-    function _getRandomActor(uint256 entropy) internal view returns (address randomActor) {
-        address[] memory actorsArray = _getActors();
-        randomActor = actorsArray[entropy % actorsArray.length];
-    }
-
-    // Get regular users
+    /// @notice Returns all actors being used
     function _getActors() internal view returns (address[] memory) {
         return _actors.values();
     }
 
-    function _enableActor(address target) internal {
-        _actor = target;
-    }
-    
-    // NOTE: disabling an actor sets the default actor (address(this)) as the current actor
-    function _disableActor() internal {
-        _actor = address(this);
-    }
-
+    /// @notice Adds an actor to the list of actors
     function _addActor(address target) internal {
         if (_actors.contains(target)) {
             revert ActorExists();
@@ -83,6 +54,7 @@ abstract contract ActorManager {
         _actors.add(target);
     }
 
+    /// @notice Removes an actor from the list of actors
     function _removeActor(address target) internal {
         if (!_actors.contains(target)) {
             revert ActorNotAdded();
@@ -92,14 +64,14 @@ abstract contract ActorManager {
             revert DefaultActor();
         }
 
-        _actors.remove(target);  
+        _actors.remove(target);
     }
 
-    // Note: expose this function _in `TargetFunctions` for actor switching
+    /// @dev Expose this in the `TargetFunctions` contract to let the fuzzer switch actors
+    /// @notice Switches the current actor based on the entropy
+    /// @param entropy The entropy to choose a random actor in the array for switching
     function _switchActor(uint256 entropy) internal {
-        _disableActor();
-
         address target = _actors.at(entropy % _actors.length());
-        _enableActor(target);
+        _actor = target;
     }
 }
--- src/Utils.sol
+++ src/Utils.sol
@@ -1,54 +1,126 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-2.0
 pragma solidity ^0.8.0;
+
+import {Panic} from "./Panic.sol";
 
 contract Utils {
 
-    function _isArithmeticOverflow(bytes memory returnData) internal pure returns (bool) {
-        return (keccak256(abi.encodePacked(_getRevertMsg(returnData))) == keccak256(abi.encodePacked("Panic(17)")));
+    /// @dev check if the error returned from a call is the same as the expected error
+    /// @param err the error returned from a call
+    /// @param expected the expected error
+    /// @return true if the error is the same as the expected error, false otherwise
+    function checkError(bytes memory err, string memory expected) internal pure returns(bool) {
+        (string memory revertMsg, bool customError) = _getRevertMsg(err);
+        
+        bytes32 errorBytes;
+        bytes32 expectedBytes;
+
+        if(customError) {
+            // Custom error returns the keccak256 hash of the error, so don't need to hash it again
+            errorBytes = bytes32(abi.encodePacked(revertMsg, bytes28(0)));
+            expectedBytes = bytes4(keccak256(abi.encodePacked(expected)));
+        } else {
+            errorBytes = keccak256(abi.encodePacked(revertMsg));
+            expectedBytes = keccak256(abi.encodePacked(expected));
+        }
+
+        // Check if error contains expected string
+        return errorBytes == expectedBytes;
     }
 
-    function _isRevertReasonEqual(bytes memory returnData, string memory reason) internal pure returns (bool) {
-        return (keccak256(abi.encodePacked(_getRevertMsg(returnData))) == keccak256(abi.encodePacked(reason)));
-    }
+    /// @dev get the revert message from a call
+    /// @notice based on https://ethereum.stackexchange.com/a/83577
+    /// @param returnData the return data from a call
+    /// @return the revert message and a boolean indicating if it's a custom error
+    function _getRevertMsg(bytes memory returnData) internal pure returns (string memory, bool) {
+        // If the returnData length is 0, then the transaction failed silently (without a revert message)
+        if (returnData.length == 0) return ("", false);
 
-    function _isRevertReasonNotEqual(bytes memory reason, string memory reasonString) internal pure returns (bool) {
-        return keccak256(reason) != keccak256(abi.encodeWithSignature(reasonString));
-    }
-
-    // https://ethereum.stackexchange.com/a/83577
-    function _getRevertMsg(bytes memory returnData) internal pure returns (string memory) {
-        // TODO: Need to check for custome errors
-
+        // 1. Panic(uint256)
         // Check that the data has the right size: 4 bytes for signature + 32 bytes for panic code
         if (returnData.length == 4 + 32) {
             // Check that the data starts with the Panic signature
-            bytes4 panicSignature = bytes4(keccak256(bytes("Panic(uint256)")));
-            for (uint256 i = 0; i < 4; i++) {
-                if (returnData[i] != panicSignature[i]) return "Undefined signature";
+            bool panic = _checkIfPanic(returnData);
+    
+            if (panic) {
+                return _getPanicCode(returnData);
             }
+        }
+        
+        // Get the error selector from returnData
+        bytes4 errorSelector = _getErrorSelector(returnData);
 
-            uint256 panicCode;
-            for (uint256 i = 4; i < 36; i++) {
-                panicCode = panicCode << 8;
-                panicCode |= uint8(returnData[i]);
+        // 2. Error(string) - If it's a standard revert string
+        bytes4 errorStringSelector = bytes4(keccak256("Error(string)")); // Get the standard Error(string) selector
+        
+        if (errorSelector == errorStringSelector) {
+            assembly {
+                // slice the sighash of the error so we can decode the string
+                returnData := add(returnData, 0x04)
             }
-
-            // Now convert the panic code into its string representation
-            if (panicCode == 17) {
-                return "Panic(17)";
-            }
-
-            // Add other panic codes as needed or return a generic "Unknown panic"
-            return "Undefined panic code";
+            return (abi.decode(returnData, (string)), false);
         }
 
-        // If the returnData length is less than 68, then the transaction failed silently (without a revert message)
-        if (returnData.length < 68) return "Transaction reverted silently";
+        // 3. Custom error - Return the custom error selector as a string
+        return (string(abi.encodePacked(errorSelector)), true);
+    }
 
+    function _checkIfPanic(bytes memory returnData) internal pure returns (bool) {
+        bytes4 panicSignature = bytes4(keccak256(bytes("Panic(uint256)")));
+        
+        for (uint256 i = 0; i < 4; i++) {
+            if (returnData[i] != panicSignature[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function _getPanicCode(bytes memory returnData) internal pure returns (string memory, bool) {
+        uint256 panicCode;
+        for (uint256 i = 4; i < 36; i++) {
+            panicCode = panicCode << 8;
+            panicCode |= uint8(returnData[i]);
+        }
+
+        // Convert the panic code into its string representation
+        if (panicCode == 1) {
+            // call assert with an argument that evaluates to false
+            return (Panic.assertionPanic, false);
+        } else if (panicCode == 17) {
+            // arithmetic operation results in underflow or overflow
+            return (Panic.arithmeticPanic, false);
+        } else if (panicCode == 18) {
+            // division or modulo by zero
+            return (Panic.divisionPanic, false);
+        } else if (panicCode == 33) {
+            // converting a value that's too big or negative into an enum type
+            return (Panic.enumPanic, false);
+        } else if (panicCode == 34) {
+            // access a storage byte array that is incorrectly encoded
+            return (Panic.arrayPanic, false);
+        } else if (panicCode == 49) {
+            // call .pop() on an empty array
+            return (Panic.emptyArrayPanic, false);
+        } else if (panicCode == 50) {
+            // array access out of bounds
+            return (Panic.outOfBoundsPanic, false);
+        } else if (panicCode == 65) {
+            // allocate too much memory or create an array that is too large
+            return (Panic.memoryPanic, false);
+        } else if (panicCode == 81) {
+            // call a zero-initialized variable of internal function type
+            return (Panic.functionPanic, false);
+        }
+
+        return ("Undefined panic code", false);
+    }
+
+    function _getErrorSelector(bytes memory returnData) internal pure returns (bytes4 errorSelector) {
         assembly {
-            // Slice the sighash.
-            returnData := add(returnData, 0x04)
+            errorSelector := mload(add(returnData, 0x20))
         }
-        return abi.decode(returnData, (string)); // All that remains is the revert string
+        return errorSelector;
     }
 }
